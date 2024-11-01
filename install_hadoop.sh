96bb1e0969d4aa1c651c8d61209c5c2e87cad13b
Katya Adishcheva <eadishcheva@gmail.com>
2024-11-01 11:30:01 +0100

    add hists install_hadoop.sh

---
+++ install_hadoop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo cp hosts /etc/hosts
+
+if [ ! -f ./hadoop-3.4.0.tar.gz ]; then
+    wget https://dlcdn.apache.org/hadoop/common/hadoop-3.4.0/hadoop-3.4.0.tar.gz
+fi
+
+tar -xvzf hadoop-3.4.0.tar.gz
+
+sudo apt-get install ansible
+sudo apt install sshpass
+
+ansible-playbook install_hadoop.yaml -vvv
+ansible-playbook start_hadoop.yaml
+
+cp workers core-site.xml hdfs-site.xml hadoop-3.4.0/etc/hadoop/
+
+cp .profile_new .profile
+source .profile
+
+sudo rm /etc/nginx/sites-enabled/nn
+sudo rm /etc/nginx/sites-enabled/ya
+sudo rm /etc/nginx/sites-enabled/dh
+
+sudo cp nn /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/nn /etc/nginx/sites-enabled/nn
+sudo systemctl reload nginx
